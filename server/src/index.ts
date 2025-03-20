c1ca5a4d48fd2c30900314e603356b516c4a7b12
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2025-03-19 19:35:27 -0600

    Backend for products

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 /* ROUTE IMPORT */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -19,7 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use("/dashboard", dashboardRoutes);
+app.use("/dashboard", dashboardRoutes); //http://localhost:8000/dashboard
+app.use("/products", productRoutes); //http://localhost:8000/products
 
 /* SERVER */
 const port = process.env.PORT || 3001;
